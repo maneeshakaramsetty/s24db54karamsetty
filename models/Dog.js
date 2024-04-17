ea60324891adab92c8b0693aff32c128333c65ce
maneesha karamsetty <karamsettymaneesha@gmail.com>
2024-04-17 17:38:10 -0500

    first

--- models/Dog.js
+++ models/Dog.js
@@ -4,7 +4,7 @@ const DogSchema = mongoose.Schema({
     Dog_Type: String,
     Name: String,
     Age: { type: Number, min: 1, max: 10 },
-    Color: { type: String, minlength: 10, maxlength: 1234 }
+    Color: { type: String, minlength: 1, maxlength: 1234 }
 });
 
 module.exports = mongoose.model("Dog", DogSchema);
