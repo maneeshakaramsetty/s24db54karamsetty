00f025ee0dc9ecd9fdeba7dd7b79d81919be65b8
maneesha karamsetty <karamsettymaneesha@gmail.com>
2024-04-17 17:16:31 -0500

    first

--- app.js
+++ app.js
@@ -3,41 +3,42 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Dog = require("./models/Dog");
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username })
-  .then(function (user){
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
+  function (username, password, done) {
+    account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
   })
-  .catch(function(err){
-  return done(err)
-  })
-  })
-  )
- 
+)
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var DogRouter = require('./routes/Dog');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
 var resourceRouter = require('./routes/resource');
- 
+
 var app = express();
- 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
- 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -46,20 +47,19 @@ app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
- 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Dog', DogRouter);
 app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
 app.use('/resource', resourceRouter);
- 
-var Dog = require("./models/Dog");
+
 require('dotenv').config();
 var mongoose = require('mongoose');
 const connectionString = process.env.MONGO_CON;
@@ -68,55 +68,67 @@ mongoose.connect(connectionString);
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded");
- 
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
+
 });
- 
+
+
+// passport config
+// Use the existing connection
+// The account model
+var account = require('./models/account');
+passport.use(new LocalStrategy(account.authenticate()));
+passport.serializeUser(account.serializeUser());
+passport.deserializeUser(account.deserializeUser())
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
- 
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
- 
+
 module.exports = app;
- 
-async function recreateDB(){
+
+async function recreateDB() {
   // Delete everything
   await Dog.deleteMany();
- 
-  let instance1 = new Dog({Dog_Type: 'Lab', Name:'Snoopy', Age: 2, Color:'Black'});
-  instance1.save().then(doc=>{
-  console.log("First object saved")}
-  ).catch(err=>{
-  console.error(err)
-  });
- 
- 
-  let instance2 = new Dog({Dog_Type: 'Husky', Name:'Popy', Age: 3, Color:'White'});
-  instance2.save().then(doc=>{
-  console.log("Second object saved")}
-  ).catch(err=>{
-  console.error(err)
-  });
- 
- 
-  let instance3 = new Dog({Dog_Type: 'Pitsbull', Name:'Rishil', Age: 1, Color:'Brown'});
-  instance3.save().then(doc=>{
-  console.log("Third object saved")}
-  ).catch(err=>{
-  console.error(err)
-  });
+
+  let instance1 = new Dog({ Dog_Type: 'Lab', Name: 'Snoopy', Age: 2, Color: 'Black' });
+  instance1.save().then(doc => {
+    console.log("First object saved")
   }
-  let reseed = true;
-  if (reseed) {recreateDB();}
+  ).catch(err => {
+    console.error(err)
+  });
+
+
+  let instance2 = new Dog({ Dog_Type: 'Husky', Name: 'Popy', Age: 3, Color: 'White' });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+
+
+  let instance3 = new Dog({ Dog_Type: 'Pitsbull', Name: 'Rishil', Age: 1, Color: 'Brown' });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
--- models/Dog.js
+++ models/Dog.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const DogSchema = mongoose.Schema({
-Dog_Type: String,
-Name: String,
-Age: Number,
-Color: String
-})
-module.exports = mongoose.model("Dog",
-DogSchema)
+    Dog_Type: String,
+    Name: String,
+    Age: { type: Number, min: 1, max: 10 },
+    Color: { type: String, minlength: 10, maxlength: 1234 }
+});
+
+module.exports = mongoose.model("Dog", DogSchema);
